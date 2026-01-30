e8f3957fc150d79af2f8b9089b0fd878e4ab5dc4
Andrew Elizabeth <2934664277@qq.com>
2026-01-30 19:25:53 +0800

    初始化 TeleportCommandsFabric 模组，添加基本文件和配置

---
+++ src/main/java/org/AndrewElizabeth/teleportcommandsfabric/Teleportcommandsfabric.java
@@ -0,0 +1,10 @@
+package org.AndrewElizabeth.teleportcommandsfabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Teleportcommandsfabric implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
