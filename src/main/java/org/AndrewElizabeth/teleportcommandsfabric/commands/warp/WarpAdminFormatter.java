f6151c478bb23295153a909dd8db66d35394e563
EasterGhost <eg051048@mail.ustc.edu.cn>
2026-03-14 10:10:20 +0800

    Fix a slight key val error

--- src/main/java/org/AndrewElizabeth/teleportcommandsfabric/commands/warp/WarpAdminFormatter.java
+++ src/main/java/org/AndrewElizabeth/teleportcommandsfabric/commands/warp/WarpAdminFormatter.java
@@ -51,8 +51,8 @@ final class WarpAdminFormatter {
 		message.append(" ")
 				.append(getTranslatedText(
 						visible
-								? "commands.teleport_commands.warpmap.globalVisible"
-								: "commands.teleport_commands.warpmap.globalHidden",
+								? "commands.teleport_commands.gwarpmap.globalVisible"
+								: "commands.teleport_commands.gwarpmap.globalHidden",
 						player)
 						.withStyle(visible ? ChatFormatting.DARK_GREEN : ChatFormatting.GRAY));
 		message.append(" ").append(toggleButton);
