2d96a2209ff2c7fa2750ba59fcbee6bbe7e2be54
EasterGhost <eg051048@mail.ustc.edu.cn>
2026-03-20 21:15:39 +0800

    Translation not found now fallback to en_us immediately

--- src/main/java/org/AndrewElizabeth/teleportcommandsfabric/utils/TranslationHelper.java
+++ src/main/java/org/AndrewElizabeth/teleportcommandsfabric/utils/TranslationHelper.java
@@ -63,17 +63,20 @@ public final class TranslationHelper {
 	private static Map<String, String> loadLanguage(String language) {
 		String filePath = String.format("/assets/%s/lang/%s.json", ASSETS_ID, language);
 		try (InputStream stream = TeleportCommands.class.getResourceAsStream(filePath)) {
-			Reader reader = new InputStreamReader(
-					Objects.requireNonNull(stream,
-							String.format("Couldn't find the required language file for \"%s\"", language)),
-					StandardCharsets.UTF_8);
+			if (stream == null) {
+				Constants.LOGGER.warn("Couldn't find the required language file for \"{}\", falling back to en_us.", language);
+				// We return the en_us map to cache it so we do not attempt to load it again to prevent an exception storm
+				return "en_us".equals(language) ? new ConcurrentHashMap<>() : TRANSLATION_CACHE.computeIfAbsent("en_us", TranslationHelper::loadLanguage);
+			}
+			Reader reader = new InputStreamReader(stream, StandardCharsets.UTF_8);
 			JsonElement json = JsonParser.parseReader(reader);
 			Map<String, String> translations = new ConcurrentHashMap<>();
 			json.getAsJsonObject().entrySet()
 					.forEach(entry -> translations.put(entry.getKey(), entry.getValue().getAsString()));
 			return translations;
 		} catch (Exception e) {
-			throw new RuntimeException("Failed to load language file: " + language, e);
+			Constants.LOGGER.warn("Failed to load language file: {}, falling back to en_us.", language, e);
+			return "en_us".equals(language) ? new ConcurrentHashMap<>() : TRANSLATION_CACHE.computeIfAbsent("en_us", TranslationHelper::loadLanguage);
 		}
 	}
 
