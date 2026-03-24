685baccfc010c7b8a426b01f84fdd3a3bb33e084
EasterGhost <eg051048@mail.ustc.edu.cn>
2026-03-24 15:21:00 +0800

    Update verison to 1.5.1

--- src/main/java/org/AndrewElizabeth/teleportcommandsfabric/Constants.java
+++ src/main/java/org/AndrewElizabeth/teleportcommandsfabric/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
 	public static final String MOD_ID = "teleport_commands_fabric";
 	public static final String ASSETS_ID = "teleport_commands_fabric";
 	public static final String MOD_NAME = "Teleport Commands Fabric";
-	public static final String VERSION = "1.5";
+	public static final String VERSION = "1.5.1";
 	public static final int CONFIG_VERSION = 2;
 	public static final int STORAGE_VERSION = 3;
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
